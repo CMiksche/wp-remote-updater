4cefdbdbb76b06caff596f3b41fd7718f5f012fa
Christoph Miksche <christoph@miksche.org>
2020-04-13 13:43:13 +0200

    feat: add pylint
    Add PyLint, gitlint and a travis file and refactor the code to pass PyLint

--- setup.py
+++ setup.py
@@ -1,34 +1,43 @@
+'''
+WP Remote Updater
+
+Copyright 2017 - 2020 Christoph Daniel Miksche
+All rights reserved.
+
+License: GNU General Public License
+'''
 import setuptools
-import updater
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
-setuptools.setup(name='wp_remote_updater',
-      version='1.0.0',
-      description='Gets the version of a reference site and update the wordpress site, if the reference site has a higher version.',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/CMiksche/wp-remote-updater',
-      author='Christoph Miksche',
-      author_email='christoph@miksche.org',
-      license='GPLv3',
-      classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-            "Operating System :: Unix",
-      ],
-      keywords=['wordpress', 'update', 'auto-update', 'upgrade'],
-      install_requires=[
-            'beautifulsoup4',
-            'lxml',
-            'mechanize',
-            'wronnay-search-lib'
-            'fire',
-            'configparser'
-      ],
-      py_modules=["updater"],
-      entry_points={
-          'console_scripts': ['wp-remote-updater=updater:main'],
-      }
-      )
+setuptools.setup(
+    name='wp_remote_updater',
+    version='1.0.0',
+    description='Gets the version of a reference site and updates the wordpress site,'
+                ' if the reference site has a higher version.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url='https://github.com/CMiksche/wp-remote-updater',
+    author='Christoph Miksche',
+    author_email='christoph@miksche.org',
+    license='GPLv3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: Unix",
+    ],
+    keywords=['wordpress', 'update', 'auto-update', 'upgrade'],
+    install_requires=[
+        'beautifulsoup4',
+        'lxml',
+        'mechanize',
+        'wronnay-search-lib'
+        'fire',
+        'configparser'
+    ],
+    py_modules=["updater"],
+    entry_points={
+        'console_scripts': ['wp-remote-updater=updater:main'],
+    }
+)
--- updater.py
+++ updater.py
@@ -1,101 +1,147 @@
 '''
 WP Remote Updater
 
-Copyright 2017, 2018 Christoph Daniel Miksche
+Copyright 2017 - 2020 Christoph Daniel Miksche
 All rights reserved.
 
 License: GNU General Public License
 '''
-from wronnay_search_lib.crawler import Crawler
+import os
 import zipfile
 import ftplib
-import os
 import shutil
-import mechanize
 import configparser
-import fire
 from urllib.request import urlopen
+import fire
+import mechanize
+from wronnay_search_lib.crawler import Crawler
 
 
-def getGenerator(site):
+def get_generator(site):
+    '''
+    Get the generator meta tag which includes the WP Version
+    :param site: The wordpress site
+    :return:
+    '''
     crawl = Crawler(site)
     meta = crawl.getMetaTags()
     return meta['generator']
 
 
+def ftp_upload(ftp, path):
+    '''
+    Upload files via ftp
+    :param ftp:
+    :param path:
+    :return:
+    '''
+    # List of local files
+    files = os.listdir(path)
+    # Change to local dir
+    os.chdir(path)
+    for file in files:
+        if os.path.isfile(file):
+            file_handler = open(file, 'rb')
+            ftp.storbinary('STOR %s' % file, file_handler)
+            file_handler.close()
+        elif os.path.isdir(file):
+            try:
+                ftp.mkd(file)
+            except (OSError, FileExistsError, IsADirectoryError, NotADirectoryError):
+                pass
+            ftp.cwd(file)
+            ftp_upload(ftp, file)
+    ftp.cwd('..')
+    os.chdir('..')
+
+
+def wp_login(config, browser):
+    '''
+    Login to WordPress
+    :param config: The config object
+    :param browser: The mechanize browser instance
+    :return:
+    '''
+    browser.open(config.get('WordPress', 'site') + '/wp-login.php')
+    browser.select_form(nr=0)
+    browser.form['user_login'] = config.get('WordPress', 'username')
+    browser.form['user_pass'] = config.get('WordPress', 'password')
+    browser.submit()
+
+
+def click_on_update(browser):
+    '''
+    Click on the update button
+    :param browser:
+    :return:
+    '''
+    # Start DB Update
+    try:
+        req = browser.click_link(text='WordPress-Datenbank aktualisieren')
+        browser.open(req)
+    except mechanize.LinkNotFoundError:
+        pass
+    try:
+        req = browser.click_link(text='Update WordPress Database')
+        browser.open(req)
+    except mechanize.LinkNotFoundError:
+        pass
+
+
 def update(settings='settings.ini'):
+    '''
+    Update the WordPress Site
+    :param settings:
+    :return:
+    '''
     # Config
     config = configparser.ConfigParser()
     config.read(settings)
-    if (getGenerator(config.get('WordPress', 'refsite')) > getGenerator(config.get('WordPress', 'site'))):
-
+    if (
+            get_generator(config.get('WordPress', 'refsite')) >
+            get_generator(config.get('WordPress', 'site'))
+    ):
         # Download Wordpress
-        f = urlopen(config.get('WordPress', 'wpurl'))
-        data = f.read()
+        file = urlopen(config.get('WordPress', 'wpurl'))
+        data = file.read()
         with open("wp.zip", "wb") as code:
             code.write(data)
 
         # Unzip WordPress
-        zip = zipfile.ZipFile('wp.zip')
-        zip.extractall()
+        zip_handler = zipfile.ZipFile('wp.zip')
+        zip_handler.extractall()
         # Remove ZIP file
         os.remove('wp.zip')
 
         # Upload to the FTP-Directory
-        myFTP = ftplib.FTP(config.get('FTP', 'server'), config.get('FTP', 'username'), config.get('FTP', 'password'))
+        my_ftp = ftplib.FTP(
+            config.get('FTP', 'server'),
+            config.get('FTP', 'username'),
+            config.get('FTP', 'password')
+        )
 
         # Change to FTP WordPress dir
-        myFTP.cwd('/'+config.get('FTP', 'workdir'))
-
-        myPath = os.getcwd()+'/wordpress/'
-        def uploadThis(path):
-            # List of local files
-            files = os.listdir(path)
-            # Change to local dir
-            os.chdir(path)
-            for f in files:
-                if os.path.isfile(f):
-                    fh = open(f, 'rb')
-                    myFTP.storbinary('STOR %s' % f, fh)
-                    fh.close()
-                elif os.path.isdir(f):
-                    try:
-                        myFTP.mkd(f)
-                    except:
-                        pass
-                    myFTP.cwd(f)
-                    uploadThis(f)
-            myFTP.cwd('..')
-            os.chdir('..')
-        uploadThis(myPath)
+        my_ftp.cwd('/'+config.get('FTP', 'workdir'))
+        my_path = os.getcwd()+'/wordpress/'
+        ftp_upload(my_ftp, my_path)
 
         # Remove WordPress Dir
         shutil.rmtree('wordpress')
 
-        # Login to WordPress
         browser = mechanize.Browser()
-        browser.open(settings.site+'/wp-login.php')
-        browser.select_form(nr = 0)
-        browser.form['user_login'] = config.get('WordPress', 'username')
-        browser.form['user_pass'] = config.get('WordPress', 'password')
-        browser.submit()
 
-        # Start DB Update
-        try:
-            req = browser.click_link(text='WordPress-Datenbank aktualisieren')
-            browser.open(req)
-        except:
-            pass
-        try:
-            req = browser.click_link(text='Update WordPress Database')
-            browser.open(req)
-        except:
-            pass
+        click_on_update(browser)
+        wp_login(config, browser)
+        click_on_update(browser)
 
 
 def main():
+    '''
+    Main function
+    :return:
+    '''
     fire.Fire(update)
 
 
 if __name__ == '__main__':
-  main()
+    main()
